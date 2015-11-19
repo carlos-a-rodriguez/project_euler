b56c32432f222ff6bd61cb32b2a0d53c7889c936
Carlos Rodriguez <charlie.mojocoa@gmail.com>
2015-11-18 21:45:39 -0500

    Added solution to problem 22

---
+++ euler_022.py
@@ -0,0 +1,29 @@
+import csv
+
+
+def total_name_score(names):
+  total_score = 0
+
+  for i in xrange(len(names)):
+    name_score = 0
+    for letter in names[i].lower():
+      name_score += ord(letter) - ord('a') + 1
+    total_score += name_score * (i + 1)
+
+  return total_score
+
+
+if __name__ == '__main__':
+  path = '/home/carlos/Downloads/p022_names.txt'
+
+  try:
+    with open(path, 'r') as f:
+      csvfile = csv.reader(f, delimiter=',')
+      names = []
+
+      for line in csvfile:
+        names.extend(line)
+
+      print total_name_score(sorted(names))
+  except IOError as e:
+    print e
