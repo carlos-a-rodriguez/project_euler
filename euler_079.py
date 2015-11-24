a5c92df8af313a04eb411391a352187117b6d541
Carlos Rodriguez <charlie.mojocoa@gmail.com>
2015-11-24 09:25:15 -0500

    Added solution to problem 79

---
+++ euler_079.py
@@ -0,0 +1,27 @@
+import networkx as nx
+
+
+def create_graph():
+  graph = nx.DiGraph()
+
+  for i in xrange(10):
+    graph.add_node(str(i))
+
+  return graph
+
+
+if __name__ == '__main__':
+  with open('supplemental_files/p079_keylog.txt', 'r') as f:
+    graph = create_graph()
+
+    for line in f:
+      numbers = line.strip()
+
+      for i in xrange(len(numbers) - 1):
+        if numbers[i + 1] not in graph[numbers[i]]:
+          graph.add_edge(numbers[i], numbers[i + 1])
+
+    top_sort = nx.algorithms.topological_sort(graph)
+
+    # remove disconnected nodes
+    print ''.join([x for x in top_sort if len(graph.in_edges(x)) != 0 or len(graph.out_edges(x)) != 0])
