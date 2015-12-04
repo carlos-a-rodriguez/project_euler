623eeb14dd72b59e90677b2e08d978d1560f778f
Carlos Rodriguez <charlie.mojocoa@gmail.com>
2015-12-04 18:28:18 -0500

    Put code in if name statement and added a comment

--- euler_059.py
+++ euler_059.py
@@ -23,22 +23,23 @@ def apply_key_to_cipher(ascii_cipher, key):
   return xor
 
 
-with open('res/p059_cipher.txt') as f:
-  cipher = f.readline().strip().split(',')
+if __name__ == '__main__':
+  with open('res/p059_cipher.txt') as f:
+    cipher = f.readline().strip().split(',')
 
-cipher = [int(x) for x in cipher]
+  cipher = [int(x) for x in cipher]
 
-results = {}
+  results = {}
 
-for perm in itertools.product(string.ascii_lowercase, repeat=3):  
-  xor = apply_key_to_cipher(cipher, ''.join(list(perm)))
-  text = ''.join([chr(x) for x in xor])
-  # split by a space to separate out the words
-  words = text.split(' ')
-  # count the number of words
-  results[''.join(list(perm))] = len(words)
+  for perm in itertools.product(string.ascii_lowercase, repeat=3):  
+    xor = apply_key_to_cipher(cipher, ''.join(list(perm)))
+    text = ''.join([chr(x) for x in xor])
+    # split by a space to separate out the words
+    words = text.split(' ')
+    # count the number of words
+    results[''.join(list(perm))] = len(words)
 
-# use the text with the most words
-code = max(results, key=results.get)
+  # use the text with the most words (may not always work on every text)
+  code = max(results, key=results.get)
 
-print sum(apply_key_to_cipher(cipher, code))
+  print sum(apply_key_to_cipher(cipher, code))
