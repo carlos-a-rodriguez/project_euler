183e9c6105a05a263171bdc7947121fbe532905f
Carlos Rodriguez <charlie.mojocoa@gmail.com>
2015-12-11 13:05:40 -0500

    Add comments

--- euler_049.py
+++ euler_049.py
@@ -1,6 +1,7 @@
 from euler_010 import sieve_of_eratosthenes
 from itertools import permutations
 
+
 if __name__ == '__main__':
   # get all four digit primes
   primes = filter(lambda x: x > 999 and x < 10000, sieve_of_eratosthenes(10000))
@@ -8,13 +9,16 @@ if __name__ == '__main__':
 
   result = set()
 
+  # find all permutations of each prime number
   for prime in primes:
     perms = set()
     for perm in permutations(str(prime)):
       perms.add(int(''.join(list(perm))))
 
+    # keep only the permutations of the prime number that are themselves prime
     perms = set(filter(lambda x: x in primes, perms))
 
+    # find primes where the difference between adjacent primes is 3330
     for p in perms:
       if p + 3330 in perms and p + 3330 * 2 in perms:
         result.add(str(p) + str(p + 3330) + str(p + 3330 * 2))
